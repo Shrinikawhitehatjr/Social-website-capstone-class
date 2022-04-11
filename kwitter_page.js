a86a971068f690fbf65b13ae83c2282c36c6dd6e
Shrinikawhitehatjr <97172285+Shrinikawhitehatjr@users.noreply.github.com>
2022-04-11 19:34:21 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,44 @@
+//YOUR FIREBASE LINKS
+
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+         firebase_message_id = childKey;
+         message_data = childData;
+         console.log(firebase_message_id);
+         console.log(message_data);
+         name = message_data('name');
+         message = message_data('message')
+         like = message_data('like')
+         name_with_tag = "<h4> "+ name +"<img class='user_tick' src='tick.png'></h4>";
+         message_with_tag = "<h4 class='messege_h4'>" + message + "<h4>";
+like_button = "<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='update like (this.id)'>";
+         span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
+
+         row = name_with_tag + message_with_tag +like_button + span_with_tag;
+         document.getElementById("output").innerHTML += row;
+         function redirectToRoomName(name)
+         {
+             console.log(name);
+             localStorage.setItem("room_name" , name);
+             window.location = "kwitter_page.html";
+         }
+
+//Start code
+   function logout() {
+         localStorage.removeItem("user_name");
+         localStorage.removeItem("room_name");
+         window.location.replace("kwitter.html");
+
+   }
+   function send()
+   {
+         msg = document.getElementById("msg").value;
+         firebase.database().ref(room_name).push({
+           name:user_name,
+           Message:msg,
+           like:0
+      });
+      document.getElementById("msg").value = "";
+   }
+//End code
+      } });  }); }
+getData();
